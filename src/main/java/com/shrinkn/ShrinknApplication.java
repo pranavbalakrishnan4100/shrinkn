7b6fb79adb572b5fbf5da9e46d4cd4d7fab7ac85
Pranav <pranav.balakrishnan@gmail.com>
2024-02-15 16:59:31 +0530

    initial commit

---
+++ src/main/java/com/shrinkn/ShrinknApplication.java
@@ -0,0 +1,13 @@
+package com.shrinkn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShrinknApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShrinknApplication.class, args);
+	}
+
+}
